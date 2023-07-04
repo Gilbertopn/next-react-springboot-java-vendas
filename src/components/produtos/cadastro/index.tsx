1d095b16fbf2bbb46e8b46201f147fe7a951dfa2
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-07-03 22:14:29 -0300

    Criação dos componentes, adicinando o Bulma CSS, criando template, menu lateral, sendo tambem criada pag de cadastro e criação do formulário

---
+++ src/components/produtos/cadastro/index.tsx
@@ -0,0 +1,54 @@
+import { Layout } from 'components'
+
+export const CadastroProdutos: React.FC = () => {
+    return(
+
+        <Layout titulo='Produtos'>
+           <div className='field'>
+                <label className='label' htmlFor='inputSKU'>SKU: *</label>
+                <div className='control'>
+                    <input className='input'
+                           id='inputSKU'
+                           placeholder='Digite o SKU do produto' />
+                </div>
+           </div>
+
+           <div className='field'>
+                <label className='label' htmlFor='inputPreco'>Preço: *</label>
+                <div className='control'>
+                    <input className='input'
+                           id='inputPreco'
+                           placeholder='Digite o Preço do  produto' />
+                </div>
+           </div>
+
+           <div className='field'>
+                <label className='label' htmlFor='inputNome'>Nome: *</label>
+                <div className='control'>
+                    <input className='input'
+                           id='inputNome'
+                           placeholder='Digite o Nome do produto' />
+                </div>
+           </div>
+
+           <div className='field'>
+                <label className='label' htmlFor='inputDesc'>Descrição: *</label>
+                <div className='control'>
+                    <textarea className='textarea'
+                           id='inputDesc'
+                           placeholder='Digite a Descrição detalhada do produto' />
+                </div>
+           </div>
+
+           <div className='field is-grouped'>
+                <div className='control is-link'>
+                    <button className='button' >Salvar</button>
+                </div>
+                <div className='control'>
+                    <button className='button' >Voltarrg</button>
+                </div>
+           </div>
+        </Layout>
+    )
+}
+
