955373d9156f0f2ba1ce03ba933f0d0b3b4c4b09
Gilberto Junior <gilbertopnjunior99@gmail.com>
2023-08-09 16:20:23 -0300

    iniciating table component

--- vendas-app/src/components/produtos/listagem/index.tsx
+++ vendas-app/src/components/produtos/listagem/index.tsx
@@ -1,14 +1,21 @@
 import { Layout } from 'components'
 import Link from 'next/link'
+import {TabelaProdutos} from './tabela'
+import {Produto} from 'app/models/produtos'
 
 export const ListagemProdutos: React.FC = () => {
+  
+    const produtos: Produto[]= [{
+        id:"1", sku: "GGGG", nome:"impressora", preco: 250.00
+    }]
+  
     return(
         <Layout titulo="Produtos">
             <Link href="/cadastros/produtos">
             <button className="button is-warning">Novo</button>
             </Link>
             <br/>
-
+            <TabelaProdutos produtos={produtos}/>
         </Layout>
 
     )
